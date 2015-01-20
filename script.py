8f519e4f960e49e1ee34f085aef8b0e5380f38a0
Fiona Leung <fionaw.leung@mail.utoronto.ca>
2015-01-19 22:30:26 -0500

    made extract that extracts html with attributes in start tag

--- script.py
+++ script.py
@@ -10,36 +10,79 @@ def makeTags(tag):
 
 
 #parse html
-def extract(str, tag):
-    if str != None:
+# extract content in multiple pairs of the given tag
+def extract(s, tag):
+    print ("***********************************************************") 
+    if s != None:
         tags = makeTags(tag)
-        innerContent = []
-        try:
-            unprocessed = str.split(tags[0])[1]
+        contents = []
+        print "tags", tags
+        print "unprocessed:", s 
+
+        #start tags may contain tag with attributes set in it, 
+        s0 = s.split(tags[0][:-1])        #split on "<tr" instead of "<tr>"
+        # assumed that html is well formed
+        del s0[0]
+        print "s0:", s0
+
+        for thing in s0:
+            print "thing: ", thing
+            if thing[0] == '>':           #start tag contains no attributes
+                print 'has no attributes in start tag'
+                thang = thing[1:].split(tags[1])[0]
+                contents.append(thang)
+                print 'thang', thang
+
+            else:                         #start tag contains attributes
+                print "has attributes in start tag"
+                thang = thing[thing.index(">")+1:].split(tags[1])[0]
+                print 'thang', thang
+                contents.append(thang)
+        print("************************************************************")
+        return contents
+
+        #unprocessed = s.split(tags[0])
+        #del unprocessed[0] 
+        #content = []
+
+        #print "unprocessed:", unprocessed
         
-        except IndexError:
-            unprocessed = str.split(tags[0][:-1])   # ele in unprocessed:
-                                                    # " align="LEFT">content</td>"
-            del unprocessed[0]
-
-            for thing in unprocessed:               # ele in unprocessed
-                thang = s0[s0.index(">") + 1:]      # "content</td>"
-                thang.split(tags[1])                # extracts "content"
-                innerContent.append(thang)
-        return innerContent
-    return str 
-#print (extract("not content<tr>hello this is the content</tr>not content", "tr"))
+        #for thing in unprocessed:
+            #print("thing: ", thing)
+            #if tags[1] in thing:
+                #print (thing.split(tags[1]))
+                #content.append(thing.split(tags[1])[0])
+        #print 'content', content
+        #return innerContent
+    return s 
+print("************************************************************")
+print('TESTING EXTRACT: ')
 #print (extract("sdf<tr>this is </tr>sdfs s fsdf <tr>the content</tr>sdfsdf", "tr"))
-#print (extract("dsfsdfsdf<b>sdf</b>jsdklfjdksfjd<table>table reached</table>sdfdsfdsf<hisdfkdfj<table>table 2 reached</table>sdfdsf<hi>Hdsfsdf</hi>", "table")[0])
-print (extract("<td>1</td><td>2</td>  <td>3</td>", "td"))
+#print (extract("dsfsdfsdf<b>sdf</b>jsdklfjdksfjd<table>table reached</table>sdfdsfdsf<hisdfkdfj<table>table 2 reached</table>sdfdsf<hi>Hdsfsdf</hi>", "table"))
+#print (extract("not content<tr>hello this is the content</tr>not content", "tr"))
+#print (extract("<td>1</td><td>2</td>  <td>3</td>", "td"))
 # will fail on tag with attributes in them
-
+#print(extract("<tr attr=12>content</tr>", "tr"))
+print(extract("<tr atrt=122 123123 12 31 23 1>one</tr><tr>two</tr>", "tr"))
+print("************************************************************")
+print('TESTING EXTRACT: ')
 
 def extract_courseCode(href):
     if (href != None):
         return href.split("#")[1]
     return href
 
+def extract_val(s):
+    i = 0
+
+    while (i < len (s)):
+        if s[i] == '>':
+            start = i
+            end = s[i+1:].index('<') + i
+            if end - start > 1:
+                return s[start+1:end+1]
+        i = i + 1
+    return s
 
 
 ''' Extracts content in a single html row(within <tr> </tr> tags).
@@ -58,14 +101,13 @@ def getRawRow(fo):
         line = fo.readline()
 
     if end in unproc:
-        return extract(unproc, tag);
+        return extract(unproc, tag)
     else:
         while (end not in line):
             unproc = unproc + line
             line = fo.readline()
-        
         unproc = unproc + line.split(end)[0] + end
-        
+
         return extract(unproc, tag)
 
     return None
@@ -92,43 +134,46 @@ LOCATION = "location"
 INSTRUCTOR = "instructor"
 
 courseInfo = {CODE: 0,
-           TITLE: 1,
-           TYPE: 2,
-           SEMESTER: 3,
-           SECTION: 4,
-           TIMES: 5,
-           LOCATION: 6, 
-           INSTRUCTOR: 7}
+        TITLE: 1,
+        TYPE: 2,
+        SEMESTER: 3,
+        SECTION: 4,
+        TIMES: 5,
+        LOCATION: 6, 
+        INSTRUCTOR: 7}
 def processRow(row, currentCourse):
-    print('in processRow')
-    print('row:')
-    print(row)
-   
 
-    
-    cells = extract(row[0], "td")
-     
-    print ("here are the cells")
+    #print('in processRow')
+    #print('row:', row)
+
+    cells = extract(row, "td")
+    #print("cells", cells)
+    #process individual cells
+
+    #print ("here are the cells")
     for cell in cells: 
         print (cell)
-    print ("no more cells")
-    print() 
+        print("cell content: ")
+        print (extract_str(cell))
+    prin ("no more cells")
+    #print() 
+
     courseCode = extract_courseCode(cells[courseInfo['code']])
     if courseCode == "":
         courseCode = currentCourse         # previously recorded course
     else: 
         currentCourse = courseCode         # new course info read
-     
+
     courseType = courseCode[-3]
 
-    return  [courseCode, \
+    return  ([courseCode, \
             cells[courseInfo['title']], \
             courseType, \
             [cells[courseInfo['semester']]], \
             [cells[courseInfo['section']]], \
             [cells[courseInfo['time']]], \
             [cells[courseInfo['location']]], \
-            [cells[courseInfo['instructor']]]], currentCourse
+            [cells[courseInfo['instructor']]]], currentCourse)
 
 
 '''
@@ -142,27 +187,27 @@ def rowToICalEvent(row):
     FRIDAY = "FR"
     SATURDAY = "SA"
     SUNDAY = "SU"
-    
+
     WEEKDAYS = [MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY]
     RAW_TO_DAY = {'M': MONDAY,
-                'T': TUESDAY,
-                'W': WEDNESDAY,
-                'R': THURSDAY,
-                'F': FRIDAY}
+            'T': TUESDAY,
+            'W': WEDNESDAY,
+            'R': THURSDAY,
+            'F': FRIDAY}
 
     timeSlots = row[TIMES]
-    
+
 
     # NOTE: UTC time format: YYYYMMDD'T'HHMM00
     START_DATE1 = "2015010"      # all events start at this month and year,
                                  # must be exact
     END_DATE = "20150401T"       # all events end/stop repeating by this date 
- 
+
      # get day number of start date
     BASE_START_DAY = 5
     startDay = BASE_START_DAY + WEEKDAYS.index(RAW_TO_DAY[timeSlots[0]])  + "T"
     startDate = START_DATE1 + startDay                         #iCal startDate
-        
+
     repDays = ""
     for char in timeSlots[:readUpTo]:
         if char == timeSlots[:-3]:
@@ -172,7 +217,7 @@ def rowToICalEvent(row):
 
     startDate = startDate + startTime
     endDate = endDate + endTime
-    
+
     # create iCal str of days and timing this event reoccurs at
     if timing[-2].isAlpha():
         startTime = chr(baseStart + timeSlot[-2]) + "00"
@@ -191,7 +236,7 @@ def rowToICalEvent(row):
     descr = row[CODE] + " - " + row[SECTION] + "\n" \
             + row[TITLE]  + "\n" + row[INSTRUCTOR] + "\n\n" \
             + row[TIMES] + "\n" + row[LOCATION]
-    
+
     # return final iCal scipt for event
     return  "\nBEGIN:VEVENT"\
             + "\nDTSTART;TZID=America/Toronto:" + startDate\
@@ -207,40 +252,39 @@ def rowToICalEvent(row):
 
 ''' do the magic.
 '''
-processedRows = []
-fi = open("csc.html", "r+")
-fo = open("output.ical", "wb")
-fo.write(CAL_START)
-row = getRawRow(fi)
+#processedRows = []
+#fi = open("csc.html", "r+")
+#fo = open("output.ical", "wb")
+#fo.write(CAL_START)
+#row = getRawRow(fi)
+##print("getting raw row", row)
 
-i = 0               # number of rows to read from input
-while (i!= 6):
-    # extract the raw html data in a single row
-    row = getRawRow(fi)
+#i = 0               # number of rows to read from input
+#while (i!= 5):
+    ## extract the raw html data in a single row
+    #row = getRawRow(fi)
+    #currentCourse = ""
+    #processedRow = []
 
-    currentCourse = ""
-    processedRow = []
-    if (i > 1): 
-        print("--------RAW")
-        print (row)
-        print ()
-        # extract data from html
-        processedRow, currentCourse = processRow(row, currentCourse)
-        print ("---PROCESSED")
-        print (processedRow)
-        print ()
-        processedRows.append(processedRow)   # remove later
-        
-        # parse data to ical
-        iCalEvent = rowToICalEvent(processedRow)
-        print ("--- PARSED ICAL")
-        print(iCalEvent)
-        print("-----------")
-        fo.write(iCalEvent)
+    #if (i > 1): 
+        ##print("--------RAW")
+        ##print (row)
+        ## extract data from html
+        #processedRow, currentCourse = processRow(row, currentCourse)
+        ##print ("---PROCESSED")
+        ##print (processedRow)
+        #processedRows.append(processedRow)   # remove later
 
-    i = i + 1
+        ## parse data to ical
+        #iCalEvent = rowToICalEvent(processedRow)
+        ##print ("--- PARSED ICAL")
+        ##print(iCalEvent)
+        ##print("-----------")
+        #fo.write(iCalEvent)
 
-fo.write(CAL_END)
+    #i = i + 1
 
-fi.close()
-fo.close()
+#fo.write(CAL_END)
+
+#fi.close()
+#fo.close()
